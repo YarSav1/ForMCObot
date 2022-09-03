edbc9ce509a26ba16c0afdc586c58393f8ca783d
chaka <yasavinih@mail.ru>
2022-09-03 14:25:20 +0500

    1

--- backend_bot/tasks_minecraft/complete_tasks/kills_complete.py
+++ backend_bot/tasks_minecraft/complete_tasks/kills_complete.py
@@ -17,22 +17,23 @@ def check_kills():
             pass
     if len(players) != 0:
         for doc in players:
-            task_kills = doc['minecraft-kills']
-            if task_kills[3] is False:
-                nick = doc["ds-minecraft"][1]
-                html = requests.get(
-                    f'https://minecraftonly.ru/index.php?player={nick}&view=search&do=tophungergames&server=0',
-                    headers=HEADERS, params=None)
-                if html.status_code == 200:
-                    html = html.text
-                    soup = BeautifulSoup(html, 'html.parser')
-                    rows = soup.find_all('tr')
-                    if len(rows) > 1:
-                        now_kills = amount_kills(rows)
-                        if now_kills-task_kills[0] >= task_kills[1]:
-                            massive = [task_kills[0], task_kills[1], task_kills[1], True]
-                        else:
-                            massive = [task_kills[0], task_kills[1], now_kills-task_kills[0], False]
-                        DB_GAME.update_one({'id_member': doc['id_member']},
-                                           {'$set': {'minecraft-kills': massive}})
+            if 'minecraft-kills' in doc:
+                task_kills = doc['minecraft-kills']
+                if task_kills[3] is False:
+                    nick = doc["ds-minecraft"][1]
+                    html = requests.get(
+                        f'https://minecraftonly.ru/index.php?player={nick}&view=search&do=tophungergames&server=0',
+                        headers=HEADERS, params=None)
+                    if html.status_code == 200:
+                        html = html.text
+                        soup = BeautifulSoup(html, 'html.parser')
+                        rows = soup.find_all('tr')
+                        if len(rows) > 1:
+                            now_kills = amount_kills(rows)
+                            if now_kills-task_kills[0] >= task_kills[1]:
+                                massive = [task_kills[0], task_kills[1], task_kills[1], True]
+                            else:
+                                massive = [task_kills[0], task_kills[1], now_kills-task_kills[0], False]
+                            DB_GAME.update_one({'id_member': doc['id_member']},
+                                               {'$set': {'minecraft-kills': massive}})
 
