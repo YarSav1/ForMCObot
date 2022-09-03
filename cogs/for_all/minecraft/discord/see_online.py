2aa76c476e3dc15a107275555376c9d0c6c4cb80
chaka <yasavinih@mail.ru>
2022-09-03 20:24:12 +0500

    1

--- cogs/for_all/minecraft/discord/see_online.py
+++ cogs/for_all/minecraft/discord/see_online.py
@@ -102,11 +102,9 @@ async def send_online(author, server, doc, max_lists, now_list):
     else:
         foring_start, foring_end = now_list*7+datetime.datetime.today().weekday(), \
                                    now_list*7+datetime.datetime.today().weekday()+7
-    days = []
-    for i in doc['every_day']:
-        days.append(i)
+    days = doc['every_day']
     print(days)
-    days = days.reverse()
+    days.reverse()
     print(days)
     text = await get_text_online(now_list, foring_start, foring_end, days=days)
     await author.send(text)
