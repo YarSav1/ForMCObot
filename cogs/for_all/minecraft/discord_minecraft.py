395fbf2407b4a358a964ab0083550e8d56b3d5bb
chaka <yasavinih@mail.ru>
2022-09-21 20:51:26 +0500

    1

--- cogs/for_all/minecraft/discord_minecraft.py
+++ cogs/for_all/minecraft/discord_minecraft.py
@@ -52,6 +52,8 @@ class success_or_no(discord.ui.View):
             title, description = f'{accept}', f'Аккаунты {self.ctx.author}-`{self.nick}` связаны!'
             await self.ctx.reply(embed=await ConnectDiscordForMinecraft(self.py).pucker(title, description,
                                                                                         SUCCESS_COLOR))
+            for btn in self.children:
+                btn.disabled = True
 
     @discord.ui.button(label="Не верно", style=discord.ButtonStyle.red)
     async def button2(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -61,6 +63,8 @@ class success_or_no(discord.ui.View):
             description = 'Вы отказались от связки "Дискорд-Майнкрафт".'
             await self.ctx.reply(embed=await ConnectDiscordForMinecraft(self.py).pucker(title, description,
                                                                                         FAILURE_COLOR))
+            for btn in self.children:
+                btn.disabled = True
 
 
 class ConnectDiscordForMinecraft(commands.Cog):
